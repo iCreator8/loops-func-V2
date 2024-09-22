234051190f0573838f35a53853baa5567cb66ef4
iCreator <iselfchallenge@gmail.com>
2024-09-22 23:11:01 +0500

    initial commit and file setup

--- exercises/e1.js
+++ exercises/e1.js
@@ -12,3 +12,5 @@ export function getFirstTenNumbers() {
 // Once you're finished run the test with "npm run test-1"
 // If the test has all tests passed, switch to the next exercise file
 // If any of the tests fails, refactor the code and run the test command after you've fixed the function
+
+initial commit
