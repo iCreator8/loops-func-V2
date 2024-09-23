be773790e3dcd93a77e8dba5e19845168769842e
iCreator <iselfchallenge@gmail.com>
2024-09-23 11:08:17 +0500

    e1.js completed

--- exercises/e1.js
+++ exercises/e1.js
@@ -3,14 +3,20 @@
 // Return an array of numbers in 1..10
 // getFirstTenNumbers() => [1, 2, 3, ... , 10]
 
-export function getFirstTenNumbers() {
+ export function getFirstTenNumbers(limit) {
   // Your code goes here...
-
+let numbers = [];
+limit = 10;
+for (let i = 1; i <= limit; i++) {
+  numbers.push(i);
 }
+return numbers;
+}
+
+// console.log(getFirstTenNumbers());
+
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-1"
 // If the test has all tests passed, switch to the next exercise file
 // If any of the tests fails, refactor the code and run the test command after you've fixed the function
-
-initial commit
