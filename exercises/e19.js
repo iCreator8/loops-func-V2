7f07e94ad7ac7889665ec45ef4d8c9e1ce78b2b4
iCreator <iselfchallenge@gmail.com>
2024-10-07 18:13:59 -0700

    e19.js completed

--- exercises/e19.js
+++ exercises/e19.js
@@ -5,7 +5,14 @@
 // NOTE: You can NOT use the array.reverse() method in your code
 
 export function reverseArray(array) {
-  // Your code goes here...
+  let reversedArray = [];
+
+  // Loop through the original array backwards
+  for (let i = array.length - 1; i >= 0; i--) {
+    reversedArray.push(array[i]);
+  }
+
+  return reversedArray;
 
 }
 
