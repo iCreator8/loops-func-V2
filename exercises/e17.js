9c58fdd4199dec273fb31bebd8157fa062c9f2a0
iCreator <iselfchallenge@gmail.com>
2024-10-07 18:00:38 -0700

    e17.js completed

--- exercises/e17.js
+++ exercises/e17.js
@@ -7,7 +7,12 @@
 
 export function doesArrayInclude(array, value) {
   // Your code goes here...
-
+for (let i = 0; i < array.length; i++) {
+    if (array[i] === value) {
+      return true;  // If the value is found, return true
+    }
+  }
+  return false;  // If the value is not found, return fals
 }
 
 
